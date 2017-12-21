6ea882bccda8c8abd0d34ced5fc567056ba978c2
David Jurelius <davan839@gmail.com>
2017-12-21 12:18:35 +0100

    added csv export#

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 require('dotenv').config();
+const json2csv = require('json2csv')
 
 const accessToken = process.env.PATREON_ACCESS_TOKEN
+const refreshToken = process.env.PATREON_REFRESH_TOKEN
+
+// TODO: get new accessToken if expired, save to .env
+
 const pageCount = 100
 const url = `https://www.patreon.com/api/oauth2/api/campaigns/1137737/pledges` +
   `?type=pledge&sort=created&page%5Bcount%5D=${pageCount}&access_token=${accessToken}`
+const csvFields = [
+  'attributes.amount_cents', 
+  'attributes.created_at', 
+  'attributes.declined_since'
+];
 
 const getPatreonData = function (url, pledges = []) {
   console.log(`Fetching: ${url}`)
@@ -23,7 +33,10 @@ const getPatreonData = function (url, pledges = []) {
 
 getPatreonData(url)
   .then(pledges => {
-    const filename = `pledges-${pledges.length}.json`
-    fs.writeFile(filename, JSON.stringify(pledges, null, 2), 'utf8', () => console.log(`We're done folks! Saved date in ${filename}.`))
+    const jsonFilename = `pledges-${pledges.length}.json`
+    const csv = json2csv({ data: pledges, fields: csvFields })
+    const csvFilename = `pledges-${pledges.length}.csv`
+    fs.writeFile(jsonFilename, JSON.stringify(pledges, null, 2), 'utf8', () => console.log(`We're done folks! Saved date in ${jsonFilename}.`))
+    fs.writeFile(csvFilename, csv, 'utf8', () => console.log(`We're done folks! Saved date in ${csvFilename}.`))
   })
   .catch(error => console.log(error))
